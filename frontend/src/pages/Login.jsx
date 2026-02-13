1dff669dc1c03ca882e7156a0f0f493d37a6702a
Kiranagouda8867 <kiranagoudanadamani@gmail.com>
2026-02-13 20:37:01 +0530

    Updated login page UI

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "../styles/Login.css";
 
 function Login() {
@@ -5,11 +6,17 @@ function Login() {
     <div className="login-page">
       <div className="login-container">
         <h2>Student Login</h2>
+
         <form>
           <input type="text" placeholder="SSP ID / Username" required />
           <input type="password" placeholder="Password" required />
           <button type="submit">Login</button>
         </form>
+
+        {/* Register Link */}
+        <p className="register-text">
+          New user? <Link to="/register">Register here</Link>
+        </p>
       </div>
     </div>
   );
